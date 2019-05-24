ef45a903c8d0ef7fab9b6de728addf6ecc325f97
Sebastien Guerin <sebast.guerin@gmail.com>
2019-05-24 14:33:19 +0200

    initial commit

---
+++ index.php
@@ -0,0 +1,75 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+    <title>Google Book API</title>
+</head>
+<body>
+
+
+<?php
+
+require './src/api.php';
+?>
+<body>
+<div class="container py-5">
+    <div class="row justify-content-center">
+        <div class="card col-10 col-md-6 px-0">
+            <div class="card-header">
+                search
+            </div>
+            <div class="card-body">
+                <form method="POST" action="index.php" class="">
+                    <div class="form-group">
+                        <label for="search">Name or ISBN</label>
+                        <input class="form-control" type="text" name="search"  id="search" required>
+                    </div>
+                    <button class="btn btn-primary justify-content-end" value="search" name="submit" type="submit">Find a book</button>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="container py-5">
+    <ul class="list-unstyled align-self-center">
+    <?php if(!empty($retour)) {
+        if (isset($retour['items'])) {
+            foreach($books as $book) {
+                ?>
+                <li class="media mb-2">
+                    <img src="<?= $book['img_couverture'] ?>" alt="..." class="img-responsive mr-3">
+                    <div class="media-body">
+                        <h3 class="flow-text"><?= $book['titre'] ?></h3>
+                        <h4 class="flow-text"><?= $book['auteur']?></h4>
+                        <ul class="list-unstyled">
+                            <li>Edition : <?= $book['edition'] ?> </li>
+                            <li>Nb de pages : <?= $book['pages'] ?> </li>
+                        </ul>
+                    </div>
+                </li>
+                <?php
+            }
+        } else {
+            ?>
+           <li class="media">No result for your search</li>
+        <?php
+        }
+    }
+    ?>
+    </ul>
+</div>
+
+
+<!-- Optional JavaScript -->
+<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+</body>
+</html>
---
+++ src/api.php
@@ -0,0 +1,58 @@
+<?php
+use GuzzleHttp\Client;
+
+require_once './vendor/autoload.php';
+
+// config to local key
+require './config/config.php';
+
+$retour ="";
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $title = $_POST['search'];
+
+
+    $client = new GuzzleHttp\Client([
+    'base_uri' => 'https://www.googleapis.com/books/v1/'
+    ]);
+    try {
+        $response = $client->request('GET', 'volumes', [
+        'query' => ['q' => $title, 'printType' => 'books'],
+        'key' => $key
+        ]);
+    }
+
+    catch (Exception $e) {
+        echo $e->getMessage();
+    }
+
+    if (isset($response) ) {
+        $retour = $response->getBody();
+
+        $retour = $retour->getContents();
+        $retour = json_decode($retour, true, 512);
+
+        if (isset($retour['items'])) {
+            $books = [];
+
+            for ($i = 0; $i < count($retour['items']); $i++) {
+                $book = $retour['items'][$i]['volumeInfo'];
+
+                $isbn = isset($book['industryIdentifiers'][0]['identifier']) ? $book['industryIdentifiers'][0]['identifier'] : 'NR';
+                $titre = isset($book['title']) ? $book['title'] : 'NR';
+                $auteur = isset($book['authors'][0]) ? $book['authors'][0] : 'NR';
+                $edition = isset($book['publisher']) ? $book['publisher'] : 'NR';
+                $thumb = isset($book['imageLinks']['thumbnail']) ? $book['imageLinks']['thumbnail'] : 'https://via.placeholder.com/130X180';
+                $pages = isset($book['pageCount']) ? $book['pageCount'] : 'NR';
+
+                $books[$i]['isbn'] = $isbn;
+                $books[$i]['titre'] = $titre;
+                $books[$i]['auteur'] = $auteur;
+                $books[$i]['edition'] = $edition;
+                $books[$i]['img_couverture'] = $thumb;
+                $books[$i]['pages'] = $pages;
+            }
+        } else {
+
+        }
+    }
+}
